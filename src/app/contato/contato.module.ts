1db6049c5c84f737d558f762d0b8f85aad5bb2e0
AFCOliveira <andrefco@outlook.com.br>
2023-05-30 10:59:43 -0300

    add ReactiveFormsModule

--- src/app/contato/contato.module.ts
+++ src/app/contato/contato.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ContatoRoutingModule } from './contato-routing.module';
 import { ContatoComponent } from './contato.component';
 import { NgxMaskModule } from 'ngx-mask';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { NgxMaskModule } from 'ngx-mask';
   imports: [
     CommonModule,
     ContatoRoutingModule,
+    ReactiveFormsModule,
     NgxMaskModule.forRoot()
   ]
 })
